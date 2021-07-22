59081e6fcfc99b96895d0e57fcb2a54497d16ee2
MagdaBer <magda.berger@gmx.de>
2021-07-22 13:32:10 +0200

    Add button folder, html and css

---
+++ src/components/button/button.stories.js
@@ -0,0 +1,9 @@
+import "./button.css";
+import button from "./button.html";
+
+export default {
+  title: "Components/Button",
+  parameters: { layout: "fullscreen" },
+};
+
+export const btnSquare = () => button;
