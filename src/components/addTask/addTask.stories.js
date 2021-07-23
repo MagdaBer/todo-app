85959e4bdd2cef23875897609ca393ff26e0c9e5
MagdaBer <magda.berger@gmx.de>
2021-07-23 13:54:14 +0200

    Add folder addTask and assets

---
+++ src/components/addTask/addTask.stories.js
@@ -0,0 +1,9 @@
+import "./addTask.css";
+import Task from "./addTask.html";
+
+export default {
+  title: "Components/addTask",
+  parameters: { layout: "fullscreen" },
+};
+
+export const Taskcontainer = () => Task;
