bc30fcf9ce011646f180275907a8882541771115
MagdaBer <magda.berger@gmx.de>
2021-07-25 17:29:49 +0200

    Add folderstructure for menu and alarm button

---
+++ src/components/buttonbell/buttonbell.stories.js
@@ -0,0 +1,9 @@
+import "./buttonbell.css";
+import buttonBell from "./buttonbell.html";
+
+export default {
+  title: "Components/Buttonbell",
+  parameters: { layout: "fullscreen" },
+};
+
+export const btnBell = () => buttonBell;
