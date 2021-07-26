712ef3b5829be36944f44121fbba4c2bea7a4811
MagdaBer <magda.berger@gmx.de>
2021-07-26 12:06:20 +0200

    Change AddTask

--- src/components/addTask/addTask.stories.js
+++
@@ -1,9 +0,0 @@
-import "./addTask.css";
-import Task from "./addTask.html";
-
-export default {
-  title: "Components/addTask",
-  parameters: { layout: "fullscreen" },
-};
-
-export const Taskcontainer = () => Task;
