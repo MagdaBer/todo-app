0ed1c49e495767d94c4a2e3c010940d69d0e8bd0
MagdaBer <magda.berger@gmx.de>
2021-07-26 12:18:10 +0200

    Add checked value in taskItemDone

--- src/components/taskItem/taskItem.stories.js
+++ src/components/taskItem/taskItem.stories.js
@@ -1,9 +1,11 @@
 import "./taskItem.css";
 import taskItemRegular from "./taskItemRegular.html";
+import taskItemDone from ".taskItemDone.html";
 
 export default {
   title: "Components/TaskItem",
   parameters: { layout: "centered" },
 };
 
-export const Taskcontainer = () => taskItemRegular;
+export const regular = () => taskItemRegular;
+export const done = () => taskItemDone;
