1b75a934d45c7f2a25385cf0755fcb0cbd517869
MagdaBer <magda.berger@gmx.de>
2021-07-26 14:36:58 +0200

    Add basicstructure taskList

---
+++ src/components/taskList/taskList.stories.js
@@ -0,0 +1,9 @@
+import "./taskList.css";
+import TaskList from "./taskList.html";
+
+export default {
+  title: "Components/taskList",
+  parameters: { layout: "fullscreen" },
+};
+
+export const List = () => TaskList;
